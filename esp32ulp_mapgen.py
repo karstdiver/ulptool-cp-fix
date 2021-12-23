351ace5f01a55f90d7ac9481c18f24f3e75c76e8
karstdiver <karstdiver@hotmail.com>
2021-12-23 10:50:23 -0500

    added code to handle b'....\r\n' formatted .sym file

--- esp32ulp_mapgen.py
+++ esp32ulp_mapgen.py
@@ -18,11 +18,40 @@ def gen_ld_h_from_sym(f_sym, f_ld, f_h):
     f_h.write("// This file is generated automatically by esp32ulp_mapgen.py utility\n\n")
     f_h.write("#pragma once\n\n")
 
+    #with open("/media/psf/Downloads/ulp_main.sym", "rb") as f:
+        #for line in f:
     for line in f_sym:
-        name, _, addr_str = line.split()
-        addr = int(addr_str, 16) + BASE_ADDR
-        f_h.write("extern uint32_t ulp_{0};\n".format(name))
-        f_ld.write("PROVIDE ( ulp_{0} = 0x{1:08x} );\n".format(name, addr))
+        f_ld.write("/* line:{0}: */\n".format(line))
+
+        if line[1] == '\'': # assume line is b'. . 12345678\r\n...' formatted
+
+        #bytes_read = f.read() # read total file into memory
+        #for b in bytes_read:
+            #process_byte(b)
+        #    f_ld.write(b)
+
+            # process each symbol
+            while len(line) >= len('n s 12345678'):  # min line is: n s 12345678
+                if line[1] == '\'': # check for /^b'/
+                    line = line[2 : ]  # shift left 2  shift off ^b'
+
+                eos = line.find('\\') # find end of symbol in string
+                name, _, addr_str = line[ : eos].split() # get symbol's values
+
+                # write symbols to ld files
+                addr = int(addr_str, 16) + BASE_ADDR
+                f_h.write("extern uint32_t ulp_{0};\n".format(name))
+                f_ld.write("PROVIDE ( ulp_{0} = 0x{1:08x} );\n".format(name, addr))
+
+                line = line[eos+len('\\r\\n') : ] # shift off just read/written symbol
+                f_ld.write("/* line:{0}: */\n".format(line))
+
+        else: # assume it is a normal input formatted line by line
+            #for line in f_sym:
+            name, _, addr_str = line.split()
+            addr = int(addr_str, 16) + BASE_ADDR
+            f_h.write("extern uint32_t ulp_{0};\n".format(name))
+            f_ld.write("PROVIDE ( ulp_{0} = 0x{1:08x} );\n".format(name, addr))
 
 
 def main():
@@ -46,7 +75,7 @@ def main():
         return 1
 
     # Dec-2021 open sym file with universal to allow for Windows vs. Unix text file formats
-    with open(options.outputfile + ".h", 'w') as f_h, open(options.outputfile + ".ld", 'w') as f_ld, open(options.symfile, 'rU') as f_sym:
+    with open(options.outputfile + ".h", 'w') as f_h, open(options.outputfile + ".ld", 'w') as f_ld, open(options.symfile, 'rb') as f_sym:
         gen_ld_h_from_sym(f_sym, f_ld, f_h)
     return 0
 
